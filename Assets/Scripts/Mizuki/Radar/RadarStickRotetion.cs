e9d699098e086c68aea658e2f2a2606ed4811a62
SasakiMizuki <mizuki.ssk.halnagoya@gmail.com>
2017-11-01 18:46:02 +0900

    仮画像

---
+++ Assets/Scripts/Mizuki/Radar/RadarStickRotetion.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RadarStickRotetion : ObjectBase {
+	[SerializeField]
+	private float m_StickRoteSpeed;
+
+	// Use this for initialization
+	void Start() {
+		m_OrderNumber = 0;
+		ObjectManager.Instance.RegistrationList(this, m_OrderNumber);
+
+	}
+
+	public override void Execute(float deltaTime) {
+		transform.Rotate(new Vector3(0.0f, 0.0f, m_StickRoteSpeed * deltaTime));
+	}
+
+	public override void LateExecute(float deltaTime) {
+
+	}
+}
