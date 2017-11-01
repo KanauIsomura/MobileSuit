a895138672db7f9893603e1f0a0baa5fa879bbd8
SasakiMizuki <mizuki.ssk.halnagoya@gmail.com>
2017-11-01 10:25:59 +0900

    プレイヤーとカメラ。暫定版

---
+++ Assets/Scripts/Mizuki/Camera/MainCamera.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCamera : ObjectBase {
+	[SerializeField]
+	private GameObject m_PlayerObj;
+	private Vector3 m_PlayerForward;
+	[SerializeField]
+	float m_CameraMoveSpeed;
+	// Use this for initialization
+	void Start() {
+		m_OrderNumber = 0;
+		ObjectManager.Instance.RegistrationList(this, m_OrderNumber);
+		m_PlayerForward = m_PlayerObj.transform.forward;
+		transform.position = -m_PlayerForward * 5.0f;
+	}
+
+	public override void Execute(float deltaTime) {
+		transform.rotation = Quaternion.Slerp(transform.rotation, m_PlayerObj.transform.rotation, Mathf.Clamp(deltaTime / m_CameraMoveSpeed, 0.0f, 1.0f));
+		m_PlayerForward = m_PlayerObj.transform.forward;
+		transform.position = m_PlayerObj.transform.position - m_PlayerForward * 5.0f;
+
+		//transform.LookAt(m_PlayerObj.transform);
+	}
+
+	public override void LateExecute(float deltaTime) {
+
+	}
+}
---
+++ Assets/Scripts/Mizuki/Player/Player.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : ObjectBase {
+	[SerializeField]
+	private HookShot hookShot;
+
+	// Use this for initialization
+	void Start() {
+		m_OrderNumber = 0;
+		ObjectManager.Instance.RegistrationList(this, m_OrderNumber);
+
+	}
+
+	public override void Execute(float deltaTime) {
+		if(MultiInput.Instance.GetTriggerButton(MultiInput.CONTROLLER_BUTTON.CIRCLE)) {
+			hookShot.Shot(transform);
+		}
+	}
+
+	public override void LateExecute(float deltaTime) {
+
+	}
+}
