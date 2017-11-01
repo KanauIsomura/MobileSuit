a333558c5d900f30a66eee67bd9f7b44b4d5e032
SasakiMizuki <mizuki.ssk.halnagoya@gmail.com>
2017-11-01 10:24:59 +0900

    キーボードで実行できるやつ

---
+++ Assets/Editor/Mizuki/QuickStarter.cs
@@ -0,0 +1,12 @@
+﻿using UnityEditor;
+
+public class QuickStarter{
+    [MenuItem("AdditionalItems/QuickStarter &s")]
+    static void AudioNameCreate() {
+        if(EditorApplication.isPlaying) {
+            EditorApplication.isPlaying = false;
+        }else {
+            EditorApplication.isPlaying = true;
+        }
+    }
+}
