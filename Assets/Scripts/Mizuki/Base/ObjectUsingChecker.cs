93361f316858baec5e81566180f4a43de5fbb3f0
SasakiMizuki <mizuki.ssk.halnagoya@gmail.com>
2017-11-02 18:45:59 +0900

    オブジェクト使用中かどうかクラスになるはず。こうご期待

---
+++ Assets/Scripts/Mizuki/Base/ObjectUsingChecker.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectUsingChecker<T> : ObjectBase {
+	private List<T> m_ObjList;
+
+	// Use this for initialization
+	void Start() {
+		m_OrderNumber = 0;
+		ObjectManager.Instance.RegistrationList(this, m_OrderNumber);
+
+	}
+
+	public override void Execute(float deltaTime) {
+
+	}
+
+	public override void LateExecute(float deltaTime) {
+
+	}
+
+	//public void 
+}
