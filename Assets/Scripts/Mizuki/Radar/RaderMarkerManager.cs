36f3d74626d592542af2177a4ec32f33d97180f7
SasakiMizuki <mizuki.ssk.halnagoya@gmail.com>
2017-11-09 17:07:51 +0900

    色々試行錯誤中。レーダー範囲外に出たら消すようにしたい

--- Assets/Scripts/Mizuki/Radar/RaderMarkerManager.cs
+++ Assets/Scripts/Mizuki/Radar/RaderMarkerManager.cs
@@ -16,21 +16,23 @@ using UnityEngine;
 
 public class RaderMarkerManager : SingletonMonoBehaviour<RaderMarkerManager> {
 	[SerializeField]
-	private List<string> m_RaderViewObjectTagList = new List<string>();
+	private List<string> m_RaderViewObjectTagList = new List<string>(); // レーダーに映すオブジェクトのタグ名リスト(Inspectorでいじること)
 	[SerializeField]
-	private GameObject m_PlayerObj;         // プレイヤーのオブジェクト
-	private ObjectUsingChecker<RaderMarker> m_MakerManager 
-		= new ObjectUsingChecker<RaderMarker>(); // レーダーに映すものリスト
+	private GameObject m_PlayerObj;             // プレイヤーのオブジェクト
+	private ObjectUsingChecker m_MakerManager;  // レーダーに映すものリスト
+    [SerializeField]
+    private GameObject m_ImagePrefub;           // マーカーアイコンプレハブ
+    public float m_RaderRange;                  // レーダーの視認範囲比率
 
 	// Use this for initialization
 	void Start () {
-	}
-	
-	// Update is called once per frame
-	void Update () {
+        m_MakerManager = new ObjectUsingChecker();
+        m_MakerManager.SetObjectParent(gameObject);
+    }
 
-	}
-
+    /// <summary>
+    /// 一定時間ごとにマーカーの設置(現状スティッククラスから呼ばれます)
+    /// </summary>
 	public void CheckAndSetMarker() {
 		List<GameObject> m_RaderObjList = new List<GameObject>();
 		m_MakerManager.DeleteObjAll();
@@ -39,9 +41,10 @@ public class RaderMarkerManager : SingletonMonoBehaviour<RaderMarkerManager> {
 		}
 
 		foreach(GameObject obj in m_RaderObjList) {
-			Vector3 position = obj.transform.position - m_PlayerObj.transform.position;
-			m_MakerManager.NewObjGet().ObjBody.SetMakerInRader(position);
-		}
+			Vector3 position = (obj.transform.position / m_RaderRange) - (m_PlayerObj.transform.position / m_RaderRange);
+			var clone = m_MakerManager.NewObjGet(m_ImagePrefub).ObjBody;
+            clone.GetComponent<RaderMarker>().SetMakerInRader(position);
+        }
 		//testImage.transform.localPosition = new Vector3(position.x, position.z, 0.0f);
 	}
 }
