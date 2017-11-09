e9ab0777b44c442f33770f4fdb6d87999d9809f1
SasakiMizuki <mizuki.ssk.halnagoya@gmail.com>
2017-11-09 17:53:18 +0900

    範囲外判定を追加

--- Assets/Scripts/Mizuki/Radar/RaderMarkerManager.cs
+++ Assets/Scripts/Mizuki/Radar/RaderMarkerManager.cs
@@ -22,12 +22,17 @@ public class RaderMarkerManager : SingletonMonoBehaviour<RaderMarkerManager> {
 	private ObjectUsingChecker m_MakerManager;  // レーダーに映すものリスト
     [SerializeField]
     private GameObject m_ImagePrefub;           // マーカーアイコンプレハブ
-    public float m_RaderRange;                  // レーダーの視認範囲比率
+    public float m_RaderRange = 1.0f;           // レーダーの視認範囲比率(大きいほうが広域が分かる)
+    [SerializeField]
+    private float m_RaderRangeLimit;            // レーダーの視認範囲(キャンバスのレーダー半径でいいです。初期値は100)
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
         m_MakerManager = new ObjectUsingChecker();
         m_MakerManager.SetObjectParent(gameObject);
+        if(m_RaderRange == 0.0f) {
+            m_RaderRange = 1.0f;
+        }
     }
 
     /// <summary>
@@ -42,8 +47,11 @@ public class RaderMarkerManager : SingletonMonoBehaviour<RaderMarkerManager> {
 
 		foreach(GameObject obj in m_RaderObjList) {
 			Vector3 position = (obj.transform.position / m_RaderRange) - (m_PlayerObj.transform.position / m_RaderRange);
+            if(m_RaderRangeLimit < Mathematics.VectorSize(new Vector3(position.x,position.z, 0.0f))) {
+                continue;
+            }
 			var clone = m_MakerManager.NewObjGet(m_ImagePrefub).ObjBody;
-            clone.GetComponent<RaderMarker>().SetMakerInRader(position);
+            clone.GetComponent<RaderMarker>().SetMakerInRader(new Vector2(position.x, position.z));
         }
 		//testImage.transform.localPosition = new Vector3(position.x, position.z, 0.0f);
 	}
