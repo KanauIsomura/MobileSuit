6395ce980bb5a377b81db0d90ac1df3400ae0a48
hal-riku-takahashi <33311100+hal-riku-takahashi@users.noreply.github.com>
2017-11-10 06:04:27 +0900

    Fish用スクリプトと魚の移動用スクリプトのファイルを作成

---
+++ Assets/Scripts/Riku/MoveBehavior/FishMoveBehavior.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace rt
+{
+    public class FishMoveBehavior : MoveBehavior
+    {
+        public override void Exec(float tick)
+        {
+            base.Exec(tick);
+        }
+    }
+}
+
---
+++ Assets/Scripts/Riku/MoveBehavior/MoveBehavior.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace rt
+{
+
+    public class MoveBehavior
+    {
+        private Transform _transform;
+        public Transform ParentTransform
+        {
+            set { this._transform = value; }
+            protected get { return this._transform; }
+        }
+
+        public virtual void Exec(float tick)
+        {
+
+        }
+
+    }
+
+}
